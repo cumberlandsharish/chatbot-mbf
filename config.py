74453aa9d214c0d1d6ad09128773e51a42a3af20
cumberlandsharish <cumberlandsharish@gmail.com>
2025-09-13 17:14:24 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 
+import os
+
+class Config:
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    PORT = int(os.environ.get("PORT", 3978))
